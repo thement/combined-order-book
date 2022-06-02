df11c8c71ebe59ca5db8813946ef523beb416851
Jakub Horak <jakub.horak@ibawizard.net>
2022-06-02 10:49:07 +0200

    Make (dummy) server compile and run

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,5 @@
 pub mod pb {
-    tonic::include_proto!("grpc.examples.echo");
+    tonic::include_proto!("orderbook");
 }
 
 use futures::Stream;
@@ -8,57 +8,25 @@ use tokio::sync::{broadcast, mpsc};
 use tokio_stream::{wrappers::ReceiverStream, StreamExt};
 use tonic::{transport::Server, Request, Response, Status, Streaming};
 
-use pb::{EchoRequest, EchoResponse};
+use pb::{Empty, Summary};
 
-type EchoResult<T> = Result<Response<T>, Status>;
-type ResponseStream = Pin<Box<dyn Stream<Item = Result<EchoResponse, Status>> + Send>>;
-
-fn match_for_io_error(err_status: &Status) -> Option<&std::io::Error> {
-    let mut err: &(dyn Error + 'static) = err_status;
-
-    loop {
-        if let Some(io_err) = err.downcast_ref::<std::io::Error>() {
-            return Some(io_err);
-        }
-
-        // h2::Error do not expose std::io::Error with `source()`
-        // https://github.com/hyperium/h2/pull/462
-        if let Some(h2_err) = err.downcast_ref::<h2::Error>() {
-            if let Some(io_err) = h2_err.get_io() {
-                return Some(io_err);
-            }
-        }
-
-        err = match err.source() {
-            Some(err) => err,
-            None => return None,
-        };
-    }
-}
+type EmptyResult<T> = Result<Response<T>, Status>;
+type OrderbookStream = Pin<Box<dyn Stream<Item = Result<Summary, Status>> + Send>>;
 
 #[derive(Debug)]
-pub struct EchoServer {
+pub struct OrderbookAggregatorServer {
     sequence_receiver: broadcast::Receiver<u32>,
     sequence_sender: broadcast::Sender<u32>,
 }
 
 #[tonic::async_trait]
-impl pb::echo_server::Echo for EchoServer {
-    async fn unary_echo(&self, _: Request<EchoRequest>) -> EchoResult<EchoResponse> {
-        Err(Status::unimplemented("not implemented"))
-    }
+impl pb::orderbook_aggregator_server::OrderbookAggregator for OrderbookAggregatorServer {
+    type BookSummaryStream = OrderbookStream;
 
-    type ServerStreamingEchoStream = ResponseStream;
-
-    async fn server_streaming_echo(
-        &self,
-        req: Request<EchoRequest>,
-    ) -> EchoResult<Self::ServerStreamingEchoStream> {
-        println!("EchoServer::server_streaming_echo");
+    async fn book_summary(&self, req: Request<Empty>) -> EmptyResult<Self::BookSummaryStream> {
+        println!("OrderbookAggregatorServer::server_streaming_echo");
         println!("\tclient connected from: {:?}", req.remote_addr());
 
-        // creating infinite stream with requested message
-        let message = req.into_inner().message;
         let mut sequence_receiver = self.sequence_sender.subscribe();
 
         // spawn and channel are required if you want handle "disconnect" functionality
@@ -68,9 +36,10 @@ impl pb::echo_server::Echo for EchoServer {
             loop {
                 match sequence_receiver.recv().await {
                     Ok(sequence_number) => {
-                        let item = EchoResponse {
-                            message: message.clone(),
-                            sequence_number,
+                        let item = Summary {
+                            spread: sequence_number as f64,
+                            bids: vec![],
+                            asks: vec![],
                         };
                         match tx.send(Result::<_, Status>::Ok(item)).await {
                             Ok(_) => {
@@ -91,67 +60,7 @@ impl pb::echo_server::Echo for EchoServer {
 
         let output_stream = ReceiverStream::new(rx);
         Ok(Response::new(
-            Box::pin(output_stream) as Self::ServerStreamingEchoStream
-        ))
-    }
-
-    async fn client_streaming_echo(
-        &self,
-        _: Request<Streaming<EchoRequest>>,
-    ) -> EchoResult<EchoResponse> {
-        Err(Status::unimplemented("not implemented"))
-    }
-
-    type BidirectionalStreamingEchoStream = ResponseStream;
-
-    async fn bidirectional_streaming_echo(
-        &self,
-        req: Request<Streaming<EchoRequest>>,
-    ) -> EchoResult<Self::BidirectionalStreamingEchoStream> {
-        println!("EchoServer::bidirectional_streaming_echo");
-
-        let mut in_stream = req.into_inner();
-        let (tx, rx) = mpsc::channel(128);
-
-        // this spawn here is required if you want to handle connection error.
-        // If we just map `in_stream` and write it back as `out_stream` the `out_stream`
-        // will be drooped when connection error occurs and error will never be propagated
-        // to mapped version of `in_stream`.
-        tokio::spawn(async move {
-            while let Some(result) = in_stream.next().await {
-                match result {
-                    Ok(v) => tx
-                        .send(Ok(EchoResponse {
-                            message: v.message,
-                            sequence_number: 55,
-                        }))
-                        .await
-                        .expect("working rx"),
-                    Err(err) => {
-                        if let Some(io_err) = match_for_io_error(&err) {
-                            if io_err.kind() == ErrorKind::BrokenPipe {
-                                // here you can handle special case when client
-                                // disconnected in unexpected way
-                                eprintln!("\tclient disconnected: broken pipe");
-                                break;
-                            }
-                        }
-
-                        match tx.send(Err(err)).await {
-                            Ok(_) => (),
-                            Err(_err) => break, // response was droped
-                        }
-                    }
-                }
-            }
-            println!("\tstream ended");
-        });
-
-        // echo just write the same data that was received
-        let out_stream = ReceiverStream::new(rx);
-
-        Ok(Response::new(
-            Box::pin(out_stream) as Self::BidirectionalStreamingEchoStream
+            Box::pin(output_stream) as Self::BookSummaryStream
         ))
     }
 }
@@ -171,10 +80,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             tokio::time::sleep(Duration::from_millis(800)).await;
         }
     });
-    let server = EchoServer { sequence_sender, sequence_receiver };
+    let server = OrderbookAggregatorServer {
+        sequence_sender,
+        sequence_receiver,
+    };
     Server::builder()
-        .add_service(pb::echo_server::EchoServer::new(server))
-        .serve("[::1]:50051".to_socket_addrs().unwrap().next().unwrap())
+        .add_service(pb::orderbook_aggregator_server::OrderbookAggregatorServer::new(server))
+        .serve("[::1]:54321".to_socket_addrs().unwrap().next().unwrap())
         .await
         .unwrap();
 
